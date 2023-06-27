5a6646db0da441222177c05748348281f825c059
Isaac955 <isaacserhane95@gmail.com>
2023-06-27 18:36:17 +0200

    update

--- script.js
+++ script.js
@@ -61,16 +61,3 @@ function recommencerTest() {
 
 document.getElementById("bouton-cps").style.cursor = "pointer";
 
-const cursor = document.querySelector('.cursor');
-
-document.addEventListener('mousemove', e => {
-    cursor.setAttribute('style', 'top:'+(e.pageY - 20)+"px; left:"+(e.pageX - 20)+"px;")
-})
-
-document.addEventListener('click', ()=>{
-    cursor.classList.add('expand');
-
-    setTimeout(()=>{
-        cursor.classList.remove("expand");
-    }, 500);
-})
